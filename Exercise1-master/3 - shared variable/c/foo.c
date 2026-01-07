2c379b2858400f9e28993778006930076809bdcb
AdinaTraaUtkilen <adinatu@stud.ntnu.no>
2026-01-07 10:49:05 +0100

    task 3 done

--- Exercise1-master/3 - shared variable/c/foo.c
+++ Exercise1-master/3 - shared variable/c/foo.c
@@ -5,28 +5,55 @@
 #include <stdio.h>
 
 int i = 0;
+pthread_mutex_t lock;
 
 // Note the return type: void*
-void* incrementingThreadFunction(){
-    // TODO: increment i 1_000_000 times
+void *incrementingThreadFunction(void *arg)
+{
+    for (int h = 0; h < 1000000; h++)
+    {
+        pthread_mutex_lock(&lock);
+        i++;
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
-void* decrementingThreadFunction(){
-    // TODO: decrement i 1_000_000 times
+void *decrementingThreadFunction(void *arg)
+{
+    for (int k = 0; k < 1000000; k++)
+    {
+        pthread_mutex_lock(&lock);
+        i--;
+        pthread_mutex_unlock(&lock);
+    }
     return NULL;
 }
 
+void *foo(void *arg)
+{
+    printf("%s\n", (char *)arg);
+    return NULL;
+}
 
-int main(){
-    // TODO: 
+int main()
+{
+    // TODO:
     // start the two functions as their own threads using `pthread_create`
     // Hint: search the web! Maybe try "pthread_create example"?
-    
+
     // TODO:
     // wait for the two threads to be done before printing the final result
-    // Hint: Use `pthread_join`    
-    
+    // Hint: Use `pthread_join`
+
+    pthread_t thread1, thread2;
+    pthread_mutex_init(&lock, NULL);
+    pthread_create(&thread1, NULL, incrementingThreadFunction, (void *)"thread1 is running");
+    pthread_create(&thread2, NULL, decrementingThreadFunction, (void *)"thread2 is running");
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_mutex_destroy(&lock);
     printf("The magic number is: %d\n", i);
     return 0;
 }
--- Exercise1-master/3 - shared variable/go/foo.go
+++ Exercise1-master/3 - shared variable/go/foo.go
@@ -3,29 +3,67 @@
 package main
 
 import (
-    . "fmt"
-    "runtime"
-    "time"
+	. "fmt"
+	"runtime"
+	// "time"
 )
 
-var i = 0
-
-func incrementing() {
-    //TODO: increment i 1000000 times
+func incrementing(ch2 chan int, done chan bool) {
+	for k := 0; k < 1_000_000; k++ {
+		ch2 <- 1
+	}
+	done <- true
+	//TODO: increment i 1000000 times
 }
 
-func decrementing() {
-    //TODO: decrement i 1000000 times
+func decrementing(ch1 chan int, done chan bool) {
+	for k := 0; k < 500_000; k++ {
+		ch1 <- 1
+	}
+	done <- true
+	//TODO: decrement i 1000000 times
+}
+
+func server(ch1 chan int, ch2 chan int, ferdig chan int, svar chan int) {
+	var i = 0
+	for {
+		select {
+		case <-ch1:
+			i--
+		case <-ch2:
+			i++
+		case <-ferdig:
+			svar <- i
+		}
+
+	}
+
 }
 
 func main() {
-    // What does GOMAXPROCS do? What happens if you set it to 1?
-    runtime.GOMAXPROCS(2)    
-	
-    // TODO: Spawn both functions as goroutines
-	
-    // We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
-    // We will do it properly with channels soon. For now: Sleep.
-    time.Sleep(500*time.Millisecond)
-    Println("The magic number is:", i)
+	// What does GOMAXPROCS do? What happens if you set it to 1?
+	runtime.GOMAXPROCS(2)
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	done := make(chan bool)
+	ferdig := make(chan int)
+	svar := make(chan int)
+
+	go server(ch1, ch2, ferdig, svar)
+
+	go incrementing(ch2, done)
+	go decrementing(ch1, done)
+	// TODO: Spawn both functions as goroutines
+
+	<-done
+	<-done
+
+	ferdig <- 1
+
+	// We have no direct way to wait for the completion of a goroutine (without additional synchronization of some sort)
+	// We will do it properly with channels soon. For now: Sleep.
+	//time.Sleep(500 * time.Millisecond)
+	resultat := <-svar
+	Println("The magic number is:", resultat)
 }
